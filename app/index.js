c5c15d6fc5126235e4b769bcb3673e5d014f7b1b
Mark Tranter <tranter.m@sky.com>
2025-02-19 08:34:56 +0000

    second

--- app/index.js
+++ app/index.js
@@ -1,26 +1,63 @@
 'use strict';
-import {} from 'dotenv/config';
+
+import { } from 'dotenv/config';
 import express from 'express';
 
-const app = express();
-const port = process.env.PORT || 3001;
+import { v4 } from 'uuid';
 
-const myLogger = (req, _, next) => {
+const port = process.env.PORT || 3001;
+const app = express();
+
+const myLogger = function(req, _, next) {
   console.log(`Incoming: ${req.url}`);
   next();
 };
 
-app.use(myLogger);
-app.use(express.static('public'));
+const makeItem = (obj) => {
+  return { ...obj, id: v4(), editing: false, v: 0 }
+}
 
-app.listen(port, (err) => {
-  if (!err) {
+let items = [makeItem({ text: "Sample", priority: "1" })];
+
+app.use(express.json());
+app.use(express.static('public'));
+app.use(myLogger);
+
+app.listen(port, (error) => {
+  if (!error) {
     console.log(`Server running on port ${port}`);
   } else {
-    console.log(err);
+    console.log(error);
   }
 });
 
-app.post('/read', (req, res) => {
-  console.log(req.body);
+app.post('/create', (req, res) => {
+  items.push(makeItem(req.body))
+  items.sort((a, b) => a.priority - b.priority);
+  res.json(items)
+});
+
+app.get('/read', (_, res) => {
+  items.sort((a, b) => a.priority - b.priority);
+  res.json(items)
+});
+
+app.post('/update', (req, res) => {
+  let update = req.body;
+  let item = items.find(e => e.id === update.id);
+  if (item.v === update.v) {
+    item.v += 1;
+    item.text = update.text;
+    item.priority = update.priority
+    items.sort((a, b) => a.priority - b.priority);
+    res.json(items)
+  }
+  else {
+    res.json({ error: "Not the latest version." })
+  }
+});
+
+app.post('/delete', (req, res) => {
+  items = items.filter(e => e.id !== req.body.id)
+  res.json(items)
 });
