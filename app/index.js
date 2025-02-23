6d7a7a8e86087c4b6f741ec5bbb1d2952c0b7370
Mark Tranter <tranter.m@sky.com>
2025-02-23 16:08:31 +0000

    error handling

--- app/index.js
+++ app/index.js
@@ -1,14 +1,25 @@
 'use strict';
 
-//import { } from 'dotenv/config';
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { myLogger, makeItem } from './helpers.js'
+//import { db, Auth } from './mongo.js';
+
+// db.once("connected", () => {
+//   Auth.findOne({ _id: "65f847f55e20aec8afd5c5f6" }).then((auth) => {
+//     console.log(auth.pwd);
+//     console.log(auth.user);
+//   }).catch(err => {
+//     console.log(err);
+//   });
+//
+// })
+
 const port = process.env.PORT || 3002;
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
 const dir = path.join(__dirname, '../public');
+
 let items = [makeItem({ text: "Use MongoDb for data persistence", priority: "1" })];
 
 const app = express();
@@ -27,30 +38,32 @@ app.listen(port, (error) => {
 app.post('/create', (req, res) => {
   items.push(makeItem(req.body))
   items.sort((a, b) => a.priority - b.priority);
-  res.json(items)
+  res.json({ items })
 });
 
 app.get('/read', (_, res) => {
   items.sort((a, b) => a.priority - b.priority);
-  res.json(items)
+  res.json({ items })
 });
 
 app.post('/update', (req, res) => {
   let update = req.body;
   let item = items.find(e => e.id === update.id);
-  if (item.v === update.v) {
+  if (!item) {
+    res.json({ items, error: "Can't find that item now." })
+  } else if (item.v === update.v) {
     item.v += 1;
     item.text = update.text;
     item.priority = update.priority
     items.sort((a, b) => a.priority - b.priority);
-    res.json(items)
+    res.json({ items })
   }
   else {
-    res.json({ error: "Not the latest version." })
+    res.json({ items, error: "Not the latest version." })
   }
 });
 
 app.post('/delete', (req, res) => {
   items = items.filter(e => e.id !== req.body.id)
-  res.json(items)
+  res.json({ items })
 });
